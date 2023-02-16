b571e302557e75475410335bcc9bb670c3f4c6f9
OKERE CHINEDUM GODWIN <nedbleezytech@gmail.com>
2023-02-16 04:37:32 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ned[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(ned);
+
+	return (0);
+}
