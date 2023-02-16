15a2de56725c72cf169525417fa8278d422cd33c
OKERE CHINEDUM GODWIN <nedbleezytech@gmail.com>
2023-02-16 11:27:23 -0800

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 but 1 this time
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+
+	return (1);
+
+
+}
