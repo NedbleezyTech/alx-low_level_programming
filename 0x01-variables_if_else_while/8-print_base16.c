c3dae92d55d3c34ed7f99b35d38a666837be9e33
OKERE CHINEDUM GODWIN <nedbleezytech@gmail.com>
2023-02-17 15:24:47 -0800

    009

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: return value 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	}
+	for (j = 'a'; j <= 'f' j++)
+	{
+	putchar(j);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
