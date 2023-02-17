f66e3ca5682bb1e8ef29ccccb7e108a25664fdbd
OKERE CHINEDUM GODWIN <nedbleezytech@gmail.com>
2023-02-17 14:47:26 -0800

    1000shit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: return value 0
+ */
+
+
+int main(void)
+{
+	int r = 0;
+
+	while (r <= 9)
+	{
+	putchar('0','\t' + r);
+	r++;
+	}
+	return (0);
+}
