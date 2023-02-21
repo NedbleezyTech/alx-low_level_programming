541ecbe4b33cb9fad7637ecc59f079e06f3ba1b0
OKERE CHINEDUM GODWIN <nedbleezytech@gmail.com>
2023-02-21 10:53:37 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,25 @@
- #include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+
+	i++;
+
+	}
+
+}
