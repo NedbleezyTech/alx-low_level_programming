1e95373ab953fb5a68da65b9bb96ced78ff9506a
OKERE CHINEDUM GODWIN <nedbleezytech@gmail.com>
2023-02-27 09:52:22 -0800

    print and copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * _strcpy - Copies the string pointed to by @src, including the
+ *           terminating null byte(\0) to the buffer pointed to by @dest.
+ * @dest: pointer to the buffer to copy the string to.
+ * @src: The string to copy
+ *
+ * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int i = 0;
 
-	for (; src[i] != '\0'; i++)
+	while (src[i])
 	{
 		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
+
